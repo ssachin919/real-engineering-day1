5eee3e8573544c98be771e78b3cdd8c6adac5b6a
ssachin919 <ssachin919@gmail.com>
2026-06-27 10:23:57 +0530

    Create course interest landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("interestBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+  message.textContent = "Thanks for showing interst! Welcome to the 30 day sprint!";
+});
