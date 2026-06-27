45537767bbfd446088840e728db9c7cda8cafc4e
ssachin919 <ssachin919@gmail.com>
2026-06-27 10:36:51 +0530

    fix spelling mistake

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.getElementById("interestBtn");
 const message = document.getElementById("message");
 
 button.addEventListener("click", function () {
-  message.textContent = "Thanks for showing interst! Welcome to the 30 day sprint!";
+  message.textContent = "Thanks for showing interest! Welcome to the 30 day sprint!";
 });
